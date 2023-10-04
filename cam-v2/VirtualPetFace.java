ed7b516394dce77f445151093af69bd8fb503c57
verycoolusername1 <gavinhw25@ausdk12.org>
2023-10-04 12:25:46 -0700

    10/4

--- cam-v2/VirtualPetFace.java
+++ cam-v2/VirtualPetFace.java
@@ -61,7 +61,7 @@ public class VirtualPetFace extends JFrame implements ActionListener{
     
     public void init() {
         String curDir = System.getProperty("user.dir");
-        
+        curDir += "/cam-v1";
         base = curDir + "/" + imageBase;
         pics = new ArrayList<Image>();
         timer = new Timer(400, this);
